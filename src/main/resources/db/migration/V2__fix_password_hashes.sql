402d156f2bfa90e529b26a9cf67ccc7dbaee13e3
saiprasanthg <saiprasanth.guthula@sjsu.edu>
2026-06-15 01:04:33 -0700

    fix: correct bcrypt password hashes for seed users

---
+++ src/main/resources/db/migration/V2__fix_password_hashes.sql
@@ -0,0 +1,3 @@
+UPDATE app_users SET password_hash = '$2a$12$Sf98jhbih7inDcWDEXQLFeM5ARWe7GwFhVYlty/x68AcKpHnu2JUG' WHERE username = 'admin';
+UPDATE app_users SET password_hash = '$2a$12$BLPWoaW0.Gcz0KqkPMvWz.KA6Na/VTDO2PchTz5Yv2mKRSFENDtKu' WHERE username = 'engineer';
+UPDATE app_users SET password_hash = '$2a$12$d3Bqb7DerlnoaFbANiRBLeWCvw2sfCofO0tU.Qv3N45XduOWPrA82' WHERE username = 'viewer';
